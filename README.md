45aec37877d24bf6506713f066aa0ce57af8135a
aurismat <aur.mat729@gmail.com>
2021-10-30 09:18:54 +0300

    wrxit: replace waxmod with wav2wax
    Replace tool with a new file that creates a new .wax file without requiring an old one

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ _(the X is x'd out)_
 ### You can find the original repo here: https://github.com/juanitogan/rbxit
 
 ## Improvements made to the original repository:
-* [x] WAX audio replacer in order to mod the audio files;
+* [x] *very* simple WAV to WAX converter;
 * [ ] Potentially other modding tools later? 
 ##### This repository only stores the Python tools used for modifications, as I have no interest in using the other things.
 This script and procedure was intended and tested with CyberStorm 1. As I could not do any extensive testing on this, YMMV and use with other games **at your own risk!**
@@ -22,10 +22,10 @@ This script and procedure was intended and tested with CyberStorm 1. As I could 
 * Install the game and get it working in your way of choice(GOG version works for this **but is not recommended**; follow original author's patcher instructions to get the abandonware .iso version working);
 * Copy the .rbx files from the game's root directory into your tools directory;
 * Using the command `py ./unrbx.py <filename.rbx>`(replacing filename with the file you want to extract, of course), extract the .rbx files into a folder(the script does it for you);
-* Copy the .WAX file you want to modify(might take some trial and error, since they have no indication as to which is which(*I might update it later with a list of files*); use `py ./wax2wav.py <file.WAX>` to get a test .wav file) from the extraction folder into the tools directory;
-* Once you have your file, get a hold of an audio file you want to patch into the file; You can and *should* export your audio files from Audacity as 'Signed 16-bit PCM';
-* Use `py ./waxmod.py <original_wax.WAX> <audio_file_you_want_to_patch_in.WAV>` and it will do it's magic; you can also reuse wax2wav to verify it;
-* Put the file you just modified back into the folder you pulled it from, and use `py ./rbx.py <folder_name>`; You should get an unnamed .RBX file inside of the folder.
+* **Backup** the WAX file you want to replace;
+* Get your WAV audio file; Audacity can help you with this, as you can just export your project as a 16-bit signed PCM;
+* Use `py ./wav2wax.py <audio_file.wav> <output_file.wax>` and you should have a new WAX file now;
+* Use your new file to replace any old file in the extracted file folders, and use `py ./rbx.py <folder_name>`; You should get an unnamed .RBX file inside of the folder.
 * Rename the new .RBX file to match the original filename and put it into the game's directory;
 * ???
 * Profit(?)
--- tools/wav2wax.py
+++ tools/wav2wax.py
@@ -1,15 +1,15 @@
 #! /usr/bin/python3
 ###############################################################################
-# waxmod.py
-# Sierra WAX audio file replacer
+# wav2wax.py
+# WAV to Sierra WAX audio converter
 # by aurismat, 2021
 # Based on mj.Jernigan's wax2wav
 #
-# Usage: py waxmod.py <orgfile.wax> <audiofile.wav>
+# Usage: py wav2wax.py <audiofile.wav> <output_file.wax>
 #
 # Requirements: SoX (Sound eXchange) tool from sox.sourceforge.net
 #
-# You can patch in your own audio into Sierra WAX audio files with this now! Beware that I have only tested this with like a few case scenarios AND with only 1 game, so YMMV.
+# You can create your own audio in Sierra WAX format now! Beware that I have only tested this with like a few case scenarios AND with only 1 game, so YMMV.
 # Audio file should be 16 bit signed PCM for consistency purposes(already provided wax2wav does that for you, or just export Audacity project as 16 bit signed PCM)
 # Huge thank you to mj.Jernigan for providing original rbx tools 
 
@@ -24,37 +24,14 @@ import subprocess
 WAX_ID    = b"WAX:"
 WAX_PCM   = 1
 WAX_ADPCM = 4
+CK_ID     = b"da"
 
 if len(sys.argv) <= 2:
-	sys.exit("Usage: vaxmod <orgfile.wax> <filename.wav>")
-waxfile = sys.argv[1]
-wavfile = sys.argv[2]
-print("patching ", wavfile, " into ", waxfile)
-#get metadata from OG file
-print("Reading original WAX file ", waxfile)
-with open(waxfile, 'rb') as f:
-    
-    headdata = f.read(30)
-    fields = struct.unpack("<4sIIHHIIHH2s", headdata)
-    waxID,               \
-        waxType,         \
-        datasize,        \
-        wFormatTag,      \
-        nChannels,       \
-        nSamplesPerSec,  \
-        nAvgBytesPerSec, \
-        nBlockAlign,     \
-        wBitsPerSample,  \
-        ckID             = fields
-        
-    if waxID != WAX_ID:
-        print(waxfile, "is not WAX audio.  Skipping.")
-        exit()
-        
-    # Read the rest of the file, then close it to rename it.
-    audiodata = f.read()
+	sys.exit("Usage: wav2wax <audiofile.wav> <output.wax>")
+wavfile = sys.argv[1]
+waxfile = sys.argv[2]
 #get data from new file
-print("Reading new audio WAV file ", wavfile)
+print("Reading audio WAV file ", wavfile)
 with open(wavfile, "rb") as ff:
     header = ff.read(44)
     headerfields = struct.unpack("<IIIIIHHIIHH4sI", header)
@@ -72,14 +49,12 @@ with open(wavfile, "rb") as ff:
         dHeader,        \
         fSize           = headerfields;
     data = ff.read()
-    print("datasize is ", fSize)
     
 #write new wax file
-print("Overwriting WAX file ", waxfile, " with new audio data")
-newlen = (len(data) - 98)
+print("Writing WAX file ", waxfile, " with audio data")
 with open(waxfile, "wb") as ff:
     headdata = struct.pack("<4sIIHHIIHH2s",
-                            waxID,
+                            WAX_ID,
                             WAX_PCM,
                             fSize,
                             1,
@@ -88,7 +63,7 @@ with open(waxfile, "wb") as ff:
                             samples*2*channels,
                             2*channels,
                             16,
-                            ckID )
+                            CK_ID )
     ff.write(headdata)
     ff.write(data)
 print("done")
